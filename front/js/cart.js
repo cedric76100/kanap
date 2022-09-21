c6a52525982ac469ad34d78f2c6da633a326b27d
cedric massalon <c.massalon@gmail.com>
2022-09-21 13:37:55 +0200

    mise en place du formulaire

--- front/js/cart.js
+++ front/js/cart.js
@@ -72,19 +72,147 @@ async function getDetailsFromServer() {
             getDetailsFromServer();
         })
     }
-    // TODO : Mettre à jour la quantité
-    function changeQuantity(id, color, qty) {
 
+    // Recupérer les boutons qtity
+    const quantityBtns = document.querySelectorAll('.itemQuantity');
+    for (const quantityBtn of quantityBtns) {
+        // On écoute le changement de valeur sur toutes les qtités
+        quantityBtn.addEventListener('change', async () => {
+            // On récupère le parent
+            const selectedArticle = quantityBtn.closest('article');
+            // On récupère l'id et la couleur stockées dans data-color et data-id
+            const updatedId = selectedArticle.getAttribute('data-id');
+            const updatedColor = selectedArticle.getAttribute('data-color');
+            // On boucle sur le cart pour savoir quelle ligne/objet éditer
+            for (const canap of cart) {
+                if (canap.id === updatedId && canap.color === updatedColor) {
+                    canap.quantity = parseInt(quantityBtn.value);
+                    // On arrete la boucle quand on a trouvé et mis à jour le bon canap : break
+                    break;
+                }
+            }
+            localStorage.setItem("cart", JSON.stringify(cart));
+            // Je rappelle la meme fonction pour mettre à jour l'affichage
+            getDetailsFromServer();
+        })
     }
-
-    document.getElementById("totalQuantity").innerHTML = qty;
-
-    // TODO : Mettre à jour le total
-    document.getElementById("totalPrice").innerHTML = totalPrice;
+    // Mettre à jour la quantité globale
+    document.getElementById("totalQuantity").textContent = qty;
+    // Mettre à jour le prix total
+    document.getElementById("totalPrice").textContent = totalPrice;
 }
-
 
 getDetailsFromServer();
 
-// TODO verif formulaire
+// formulaire
 
+// sélection du bouton Valider
+const btnValidate = document.querySelector("#order");
+
+// Écoute du bouton Valider sur le click pour pouvoir valider le formulaire
+btnValidate.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    let contact = {
+        firstName: document.querySelector("#firstName").value,
+        lastName: document.querySelector("#lastName").value,
+        address: document.querySelector("#address").value,
+        city: document.querySelector("#city").value,
+        email: document.querySelector("#email").value,
+    };
+
+
+    // contrôle des champs Prénom, Nom et Ville
+    const regExPrenomNomVille = (value) => {
+        return /^[A-Z][A-Za-z\é\è\ê\-]+$/.test(value);
+    };
+
+    // contrôle du champ Adresse
+    const regExAdresse = (value) => {
+        return /^[a-zA-Z0-9.,-_ ]{5,50}[ ]{0,2}$/.test(value);
+    };
+
+    // contrôle du champ Email
+    const regExEmail = (value) => {
+        return /^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$/.test(
+            value
+        );
+    };
+
+    // Fonctions de contrôle du champ Prénom:
+    function firstNameControl() {
+        const prenom = contact.firstName;
+        if (regExPrenomNomVille(prenom)) {
+            document.querySelector("#firstNameErrorMsg").textContent = "";
+            return true;
+        } else {
+            document.querySelector("#firstNameErrorMsg").textContent =
+                "Champ Prénom de formulaire invalide, ex: Jules";
+            return false;
+        }
+    }
+
+    // Fonctions de contrôle du champ Nom:
+    function lastNameControl() {
+        const nom = contact.lastName;
+        if (regExPrenomNomVille(nom)) {
+            document.querySelector("#lastNameErrorMsg").textContent = "";
+            return true;
+        } else {
+            document.querySelector("#lastNameErrorMsg").textContent =
+                "Champ Nom de formulaire invalide, ex: Durant";
+            return false;
+        }
+    }
+
+    // Fonctions de contrôle du champ Adresse:
+    function addressControl() {
+        const adresse = contact.address;
+        if (regExAdresse(adresse)) {
+            document.querySelector("#addressErrorMsg").textContent = "";
+            return true;
+        } else {
+            document.querySelector("#addressErrorMsg").textContent =
+                "Champ Adresse de formulaire invalide, ex: 20 rue de la gare";
+            return false;
+        }
+    }
+
+    // Fonctions de contrôle du champ Ville:
+    function cityControl() {
+        const ville = contact.city;
+        if (regExPrenomNomVille(ville)) {
+            document.querySelector("#cityErrorMsg").textContent = "";
+            return true;
+        } else {
+            document.querySelector("#cityErrorMsg").textContent =
+                "Champ Ville de formulaire invalide, ex: Paris";
+            return false;
+        }
+    }
+
+    // Fonctions de contrôle du champ Email:
+    function mailControl() {
+        const courriel = contact.email;
+        if (regExEmail(courriel)) {
+            document.querySelector("#emailErrorMsg").textContent = "";
+            return true;
+        } else {
+            document.querySelector("#emailErrorMsg").textContent =
+                "Champ Email de formulaire invalide, ex: exemple@contact.fr";
+            return false;
+        }
+    }
+
+    // Contrôle validité formulaire avant de l'envoyer dans le local storage
+    if (
+        firstNameControl() &&
+        lastNameControl() &&
+        addressControl() &&
+        cityControl() &&
+        mailControl()
+    ) {
+        // Enregistrer le formulaire dans le local storage
+        localStorage.setItem("contact", JSON.stringify(contact));
+    }
+});
